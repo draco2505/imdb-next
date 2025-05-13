4fb42ec786a3d159c6051bb7a6cd2011386f5bb0
draco2505 <draco02@live.com.mx>
2025-05-13 14:02:19 -0600

    add loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+
+
+const loading = () => {
+  return (
+    <div className="flex justify-center ">
+      <img className="h-60" src="spinner.svg" alt="Loading..." />
+    </div>
+  )
+}
+
+export default loading
--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,19 @@ const TOKEN_API = process.env.TOKEN_API;
 
 const Home = async ( { searchParams } ) => {
   const genre = searchParams.genre || 'fetchTrending';
-  const res = await fetch(`https://api.themoviedb.org/3${
-    genre === 'fetchTopRated' ? '/movie/top_rated' : '/trending/all/week'
-    }?api_key=${API_KEY}&language=en-US&page=1`, {
-    next: { revalidate: 60 }
-  });
+  const res = await new Promise((resolve) => { 
+    setTimeout(async () => {
+    const response = await fetch(`https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? '/movie/top_rated' : '/trending/all/week'
+      }?api_key=${API_KEY}&language=en-US&page=1`, {
+      next: { revalidate: 60 }
+    });
+    resolve(response);
+  }, 2000);
+  })
+
+
+ 
 
   const data = await res.json();
   console.log(res)
